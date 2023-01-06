3458fcf2ba8b08b1fe0b2a83bf7f48dc6b9e1b8e
jserrao24 <jared.serrao5@gmail.com>
2023-01-05 21:32:17 -1000

    test

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,13 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.Write("What is your frist name? ");
+        string  firstName= Console.ReadLine();
+
+        Console.Write("What is your last name? ");
+        string  lastName= Console.ReadLine();
+
+        Console.WriteLine($"It is {lastName}, {firstName} {lastName}");
+
     }
 }
