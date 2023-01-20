dbde686d077c0c7f2596fb068904d9c1f1f01b6b
jserrao24 <jared.serrao5@gmail.com>
2023-01-19 22:08:42 -1000

    test

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -17,11 +17,11 @@ class Program
         job2._endYear = 2023;
 
         Resume myResume = new Resume();
-        myResume._name = "Allison Rose";
 
         myResume._jobs.Add(job1);
         myResume._jobs.Add(job2);
 
         myResume.Display();
+       
     }
 }
