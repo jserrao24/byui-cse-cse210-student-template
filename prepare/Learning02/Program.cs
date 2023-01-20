76f3d70e30837ffa72d4dc1b3cda860d577285f4
jserrao24 <jared.serrao5@gmail.com>
2023-01-19 21:35:36 -1000

    test

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,34 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
+        Job job1 = new Job();
+        job1._jobTitle = "Software Engineer";
+        job1._company = "Microsoft";
+        job1._startYear = 2019;
+        job1._endYear = 2022;
 
-    // A code template for the category of things known as Person. The
-    // responsibility of a Person is to hold and display personal information.
-    public class Person
-    {
-        // The C# convention is to start member variables with an underscore _
-        public string _givenName = "";
-        public string _familyName = "";
+        Job job2 = new Job();
+        job2._jobTitle = "Manager";
+        job2._company = "Apple";
+        job2._startYear = 2022;
+        job2._endYear = 2023;
 
-        // A special method, called a constructor that is invoked using the  
-        // new keyword followed by the class name and parentheses.
-        public Person()
-        {
-        }
+        Resume myResume = new Resume();
+        myResume._name = "Allison Rose";
 
-        // A method that displays the person's full name as used in eastern 
-        // countries or <family name, given name>.
-        public void ShowEasternName()
-        {
-            Console.WriteLine($"{_familyName}, {_givenName}");
-        }
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
 
-        // A method that displays the person's full name as used in western 
-        // countries or <given name family name>.
-        public void ShowWesternName()
-        {
-            Console.WriteLine($"{_givenName} {_familyName}");
-        }
-    };
+        myResume.Display();
     }
 }
