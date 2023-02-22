2faafdcf6944657985918de6393b97543e7ab8bb
jserrao24 <jared.serrao5@gmail.com>
2023-02-21 21:34:00 -1000

    test

--- prove/Develop04/Program.cs
+++ prove/Develop04/Program.cs
@@ -1,9 +1,47 @@
 using System;
+using System.Collections.Generic;
+using MindfulnessApp;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop04 World!");
+        // Welcome message
+        Console.WriteLine("Welcome to the Mindfulness App!");
+
+        while (true)
+        {
+            // Display main menu and get user input
+            Console.WriteLine("\nPlease choose an activity:");
+            Console.WriteLine("1. Breathing Activity");
+            Console.WriteLine("2. Reflection Activity");
+            Console.WriteLine("3. Exit");
+            Console.Write("Enter your choice: ");
+            string input = Console.ReadLine();
+
+            if (input == "1")
+            {
+                // Run breathing activity
+                BreathingActivity breathingActivity = new BreathingActivity(60);
+                breathingActivity.RunActivity();
+            }
+            else if (input == "2")
+            {
+                // Run reflection activity
+                ReflectionActivity reflectionActivity = new ReflectionActivity(120);
+                reflectionActivity.RunActivity();
+            }
+            else if (input == "3")
+            {
+                // Exit the app
+                Console.WriteLine("Thank you for using the Mindfulness App. Goodbye!");
+                break;
+            }
+            else
+            {
+                // Invalid input
+                Console.WriteLine("Invalid input. Please enter a valid choice.");
+            }
+        }
     }
 }
---
+++ prove/Develop04/classes.cs
@@ -0,0 +1,113 @@
+using System;
+using System.Collections.Generic;
+
+namespace MindfulnessApp
+{
+    // Parent class for all mindfulness activities
+    class MindfulnessActivity
+    {
+        protected int duration;
+        protected string description;
+
+        // Common starting message for all activities
+        public void StartActivity()
+        {
+            Console.WriteLine("Starting " + description);
+            Console.WriteLine("Duration: " + duration + " seconds");
+            Console.WriteLine("Get ready to begin in 3 seconds...");
+            System.Threading.Thread.Sleep(3000);
+        }
+
+        // Common ending message for all activities
+        public void EndActivity()
+        {
+            Console.WriteLine("Good job! You have completed " + description);
+            Console.WriteLine("Duration: " + duration + " seconds");
+            Console.WriteLine("Returning to main menu in 3 seconds...");
+            System.Threading.Thread.Sleep(3000);
+        }
+
+        // Common function to show spinner animation
+        protected void ShowSpinner(int seconds)
+        {
+            for (int i = 0; i < seconds; i++)
+            {
+                Console.Write("/");
+                System.Threading.Thread.Sleep(1000);
+                Console.Write("\b-");
+                System.Threading.Thread.Sleep(1000);
+                Console.Write("\b\\");
+                System.Threading.Thread.Sleep(1000);
+                Console.Write("\b|");
+                System.Threading.Thread.Sleep(1000);
+            }
+            Console.Write("\b");
+        }
+    }
+
+    // Breathing Activity class
+    class BreathingActivity : MindfulnessActivity
+    {
+        // Constructor to set duration and description
+        public BreathingActivity(int duration)
+        {
+            this.duration = duration;
+            this.description = "Breathing Activity";
+        }
+
+        // Function to run the activity
+        public void RunActivity()
+        {
+            StartActivity();
+
+            Console.WriteLine("This activity will help you relax by walking you through breathing in and out slowly. Clear your mind and focus on your breathing.");
+
+            int count = duration / 4; // Display "Breathe in..." and "Breathe out..." each for 4 seconds
+            for (int i = 0; i < count; i++)
+            {
+                Console.WriteLine("Breathe in...");
+                ShowSpinner(4);
+                Console.WriteLine("Breathe out...");
+                ShowSpinner(4);
+            }
+
+            EndActivity();
+        }
+    }
+
+    // Reflection Activity class
+    class ReflectionActivity : MindfulnessActivity
+    {
+        private List<string> prompts;
+        private List<string> questions;
+
+        // Constructor to set duration, description, prompts and questions
+        public ReflectionActivity(int duration)
+        {
+            this.duration = duration;
+            this.description = "Reflection Activity";
+            this.prompts = new List<string> { "Think of a time when you stood up for someone else.", "Think of a time when you did something really difficult.", "Think of a time when you helped someone in need.", "Think of a time when you did something truly selfless." };
+            this.questions = new List<string> { "Why was this experience meaningful to you?", "Have you ever done anything like this before?", "How did you get started?", "How did you feel when it was complete?", "What made this time different than other times when you were not as successful?", "What is your favorite thing about this experience?", "What could you learn from this experience that applies to other situations?", "What did you learn about yourself through this experience?", "How can you keep this experience in mind in the future?" };
+        }
+
+        // Function to run the activity
+        public void RunActivity()
+        {
+            StartActivity();
+
+            Console.WriteLine("This activity will help you reflect on times in your life when you have been brave, kind, or selfless. Take a moment to think about each prompt and answer the corresponding question.");
+                    foreach (string prompt in prompts) 
+        {
+            Console.WriteLine(prompt);
+            System.Threading.Thread.Sleep(5000); // Give user 5 seconds to think about prompt
+
+            foreach (string question in questions) 
+            {
+                Console.WriteLine(question);
+                System.Threading.Thread.Sleep(3000); // Give user 3 seconds to answer question
+            }
+        }
+
+        EndActivity();
+    }
+}
