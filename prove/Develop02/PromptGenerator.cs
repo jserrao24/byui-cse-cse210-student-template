2c43c584a4d1f7498dbfbe67ece79bdf7d41944b
jserrao24 <jared.serrao5@gmail.com>
2023-03-14 23:32:09 -1000

    test

--- prove/Develop02/PromptGenerator.cs
+++ prove/Develop02/PromptGenerator.cs
@@ -1,6 +1,42 @@
 
+using System;  
+using System.Collections.Generic;  
+
+public class PromptGenerator{
+
+        public PromptGenerator();
+                private List<string> prompts = new List<string>();
+                {
+                "Who was the most interesting person I interacted with today?",
+                "What was the best part of my day?",
+                 "How did I see the hand of the Lord in my life today?",
+                 "What was the strongest emotion I felt today?",
+                "If I had one thing I could do over today, what would it be?"
+                 };              
+
+}
+
+
+
 
 /*
-        States/variable/attributes;
+
+
+    public void AddPrompt(string prompt)
+    {
+        // Add a custom prompt to the list
+        prompts.Add(prompt);
+    }
+
+    public string GetRandomPrompt()
+    {
+        // Generate a random prompt from the list
+        Random rand = new Random();
+        int index = rand.Next(prompts.Count);
+        return prompts[index];
+    }
+}      States/variable/attributes;
+
+        Does a PromptGenerator need to store anything?
 
         */
