6d78a77fac8667bb36e0529d2398af09521e7695
jserrao24 <jared.serrao5@gmail.com>
2023-04-01 18:20:30 -1000

    test

---
+++ final/Foundation1/Videoclass.cs
@@ -0,0 +1,40 @@
+class Video
+{
+    public string Title { get; }
+    public string Author { get; }
+    public int Length { get; }
+    public List<Comment> Comments { get; }
+
+    public Video(string title, string author, int length)
+    {
+        Title = title;
+        Author = author;
+        Length = length;
+        Comments = new List<Comment>();
+    }
+
+    public void AddComment(string name, string text)
+    {
+        Comment comment = new Comment(name, text);
+        Comments.Add(comment);
+    }
+
+    public int GetNumberOfComments()
+    {
+        return Comments.Count;
+    }
+}
+
+class Comment
+{
+    public string Name { get; }
+    public string Text { get; }
+
+    public Comment(string name, string text)
+    {
+        Name = name;
+        Text = text;
+    }
+}
+
+This program creates a list of videos, adds 3-4 comments to each video, and then iterates through the list to display the video information and comments for each video.
